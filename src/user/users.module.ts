3dd7aa510054a4aac0930d72a045c3ffcae5ef31
Stanislav Minko <minkostas@gmail.com>
2024-07-13 11:03:34 +0300

    commit_1

--- src/user/users.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UsersController } from './users.controllers';
-import { UsersService } from './users.service';
-
-@Module({
-  controllers: [UsersController],
-  providers: [UsersService]
-})
-export class UsersModule {}
